89cc729d601632b6be58cfd7f3ec99effe2ac983
ntuthuko <ntuthun836@gmail.com>
2025-04-23 23:03:35 +0200

    Added a scripting

---
+++ script2.js
@@ -0,0 +1,7 @@
+/*============ typing animation =============*/
+var typed = new Typed(".typing", {
+    strings:["Data Scientist", "ML Engineer", "Data Modeller", "Data Aanalyst"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
