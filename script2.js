1ba00e7dbe509c36b644817d9254ee77b7b4ff6c
ntuthuko <ntuthun836@gmail.com>
2025-04-25 10:42:46 +0200

    Script added

--- script2.js
+++ script2.js
@@ -5,3 +5,33 @@ var typed = new Typed(".typing", {
     Backspeed:60,
     loop:true
 })
+
+/*============ Aside loop tab touch loop=============*/
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function()
+        {
+            for(let j=0; j<totalNavList; j++)
+            {
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+        })
+    }
+
+    function showSection(element)
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+    }
